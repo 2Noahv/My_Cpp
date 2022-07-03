28b9d4a2cfaec73dc06aaff561802877ffdfb6f7
2Noahv <w35682@naver.com>
2022-07-03 14:37:12 +0900

    basics

--- grammar/분할구현/func.cpp
+++ grammar/분할구현/func.cpp
@@ -2,7 +2,7 @@
 #include "func.h"
 // func.h 참조한다. (복사 붙여넣기)
 
-int Add(int a, int b);
+int Add(int a, int b)
 {
     return a + b;
 }
---
+++ grammar/분할구현/func.h
@@ -0,0 +1,5 @@
+// func.h
+#pragma once
+
+int Add(int a, int b);
+// 이런 함수가 있어요 ~ 라고 선언만 한 것
